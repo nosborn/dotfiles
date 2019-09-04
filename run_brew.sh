e753b399140bb2ce0c234cad1d64d2612a8b81ae
Nick Osborn <nosborn@noreply.users.github.com>
2019-09-04 11:02:10 +0800

    Update

--- run_brew.sh
+++ run_brew.sh
@@ -10,4 +10,9 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_INSECURE_REDIRECT=1
 
 brew analytics off
+
+if [ "$(hostname -s)" = 19SGD-MAC11 ]; then
+  export ACCEPT_EULA=Y
+  export HOMEBREW_NO_ENV_FILTERING=1 # https://github.com/microsoft/homebrew-mssql-release/pull/26
+fi
 brew bundle --global
