72a4b50eace1485ce0870189fec7ffb1a4d2ef29
Nick Osborn <nosborn@noreply.users.github.com>
2019-09-16 19:18:18 +0800

    Vim -> Neovim

--- run_npm.sh
+++ run_npm.sh
@@ -5,7 +5,7 @@ set -o nounset
 
 command -v npm >/dev/null 2>&1 || exit
 
-for p in markdownlint-cli; do
+for p in markdownlint-cli neovim; do
   npm list --global "${p}" >/dev/null && continue
   npm install --global --no-optional --no-package-lock --no-update-notifier --only=production "${p}"
 done
