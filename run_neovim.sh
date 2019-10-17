09a15ff97f53b6b035fd9c4c9d219901e08dba48
Nick Osborn <nosborn@noreply.users.github.com>
2019-10-17 18:33:45 +0800

    Update

--- run_neovim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -o errexit
-set -o nounset
-
-for add in ~/.config/nvim/spell/*.add; do
-  nvim -c "mkspell! ${add}" -c 'quit'
-done
--- run_vim.sh
+++ run_vim.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o nounset
 
 for add in ~/.vim/spell/*.add; do
-  nvim -c "mkspell! ${add}" -c 'quit'
+  vim -c "mkspell! ${add}" -c 'quit'
 done
