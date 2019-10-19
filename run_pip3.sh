1fb788516b023b3cfad859906498137725a458d4
Nick Osborn <nosborn@noreply.users.github.com>
2019-10-19 19:29:22 +0800

    Ansible from pip

--- run_pip3.sh
+++ run_pip3.sh
@@ -4,16 +4,17 @@ set -o errexit
 set -o nounset
 
 python3 -mpip install --upgrade --quiet --disable-pip-version-check \
+  ansible \
+  ansible-lint \
+  datadog \
+  netaddr \
+  molecule \
   vim-vint
 
 if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
   python3 -mpip install --upgrade --quiet --disable-pip-version-check \
-    ansible \
-    ansible-lint \
     azure \
-    datadog \
     jinja2 \
-    molecule \
     pykeepass \
     pylint \
     pyaml
