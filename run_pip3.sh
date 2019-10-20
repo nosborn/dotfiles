32c0e5a0806260684eff7cef628e2dcd7f903e12
Nick Osborn <nosborn@noreply.users.github.com>
2019-10-21 07:05:44 +0800

    Update

--- run_pip3.sh
+++ run_pip3.sh
@@ -7,6 +7,7 @@ python3 -mpip install --upgrade --quiet --disable-pip-version-check \
   ansible \
   ansible-lint \
   datadog \
+  jinja2 \
   netaddr \
   molecule \
   vim-vint
@@ -14,8 +15,6 @@ python3 -mpip install --upgrade --quiet --disable-pip-version-check \
 if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
   python3 -mpip install --upgrade --quiet --disable-pip-version-check \
     azure \
-    jinja2 \
     pykeepass \
-    pylint \
     pyaml
 fi
