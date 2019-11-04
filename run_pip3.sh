818fd49534124c1cfe1208a4fa72ec5b41d5a176
Nick Osborn <nosborn@users.noreply.github.com>
2019-11-04 11:12:55 +0800

    Update private_dot_Brewfile.tmpl Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -3,17 +3,15 @@
 set -o errexit
 set -o nounset
 
-python3 -mpip install --upgrade --quiet --disable-pip-version-check \
-  black \
-  datadog \
-  jinja2 \
-  netaddr \
-  molecule \
-  vim-vint
+# python3 -mpip install --upgrade --quiet --disable-pip-version-check \
+#   datadog \
+#   jinja2 \
+#   netaddr \
+#   vim-vint
 
-if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
-  python3 -mpip install --upgrade --quiet --disable-pip-version-check \
-    azure \
-    pykeepass \
-    pyaml
-fi
+# if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
+#   python3 -mpip install --upgrade --quiet --disable-pip-version-check \
+#     azure \
+#     pykeepass \
+#     pyaml
+# fi
