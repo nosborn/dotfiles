5ee80bd6f30d4e35da06e5ae0779d4938286e8cd
Nick Osborn <nosborn@noreply.users.github.com>
2019-11-05 18:42:50 +0800

    Update

--- run_neovim.sh
+++ run_neovim.sh
@@ -3,6 +3,15 @@
 set -o errexit
 set -o nounset
 
+nvim -n -u NONE +"helptags ALL" +'quit'
+
 for add in ~/.config/nvim/spell/*.add; do
-  nvim -c "mkspell! ${add}" -c 'quit'
+  nvim -n -u NONE +"mkspell! ${add}" +'quit'
 done
+
+yarn install \
+  --cwd ~/.config/nvim/pack/iamcco/start/markdown-preview.nvim/app \
+  --emoji false \
+  --no-default-rc \
+  --non-interactive \
+  --production
