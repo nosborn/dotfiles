1b70c761ab444d59cf752f093704e2cbcd8caac1
Nick Osborn <nosborn@noreply.users.github.com>
2019-11-09 07:51:02 +0800

    Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -9,7 +9,6 @@ python3 -mpip install --upgrade --quiet --disable-pip-version-check \
 #   datadog \
 #   jinja2 \
 #   netaddr \
-#   vim-vint
 
 # if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
 #   python3 -mpip install --upgrade --quiet --disable-pip-version-check \
