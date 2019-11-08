cb23a843910ed2e8c3919b2ea3c78763b5ba4a4a
Nick Osborn <nosborn@users.noreply.github.com>
2019-11-08 16:34:48 +0800

    Remove .gitmodules Remove private_dot_config/nvim/exact_pack/airblade/start/vim-gitgutter Remove private_dot_config/nvim/exact_pack/cespare/start/vim-toml Remove private_dot_config/nvim/exact_pack/chriskempson/start/base16-vim Remove private_dot_config/nvim/exact_pack/dense-analysis/start/ale Remove private_dot_config/nvim/exact_pack/digitaltoad/start/vim-pug Remove private_dot_config/nvim/exact_pack/direnv/start/direnv.vim Remove private_dot_config/nvim/exact_pack/godlygeek/start/tabular Remove private_dot_config/nvim/exact_pack/hashivim/start/vim-terraform Remove private_dot_config/nvim/exact_pack/iamcco/start/markdown-preview.nvim Remove private_dot_config/nvim/exact_pack/junegunn/start/fzf.vim Remove private_dot_config/nvim/exact_pack/pangloss/start/vim-javascript Remove private_dot_config/nvim/exact_pack/pearofducks/start/ansible-vim Remove private_dot_config/nvim/exact_pack/tpope/start/vim-commentary Remove private_dot_config/nvim/exact_pack/tpope/start/vim-fugitive Remove private_dot_config/nvim/exact_pack/vim-airline/start/vim-airline Remove private_dot_config/nvim/exact_pack/vim-airline/start/vim-airline-themes Update private_dot_config/nvim/init.vim Update run_neovim.sh

--- run_neovim.sh
+++ run_neovim.sh
@@ -8,10 +8,3 @@ nvim -n -u NONE +"helptags ALL" +'quit'
 for add in ~/.config/nvim/spell/*.add; do
   nvim -n -u NONE +"mkspell! ${add}" +'quit'
 done
-
-yarn install \
-  --cwd ~/.config/nvim/pack/iamcco/start/markdown-preview.nvim/app \
-  --emoji false \
-  --no-default-rc \
-  --non-interactive \
-  --production
