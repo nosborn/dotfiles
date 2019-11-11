987ff3b779c518b22c00145644aaf446d4184763
Nick Osborn <nosborn@users.noreply.github.com>
2019-11-11 08:31:46 +0800

    Update

--- run_brew.sh
+++ run_brew.sh
@@ -15,3 +15,9 @@ if [ "$(hostname -s)" = 19SGD-MAC11 ]; then
   export HOMEBREW_NO_ENV_FILTERING=1 # https://github.com/microsoft/homebrew-mssql-release/pull/26
 fi
 brew bundle --global
+
+for dylib in libcrypto.dylib libssl.dylib; do
+  [ -e "$(brew --prefix openssl)/lib/${dylib}" ] || continue
+  [ -e "$(brew --prefix)/lib/${dylib}" ] && continue
+  ln -s "$(brew --prefix openssl)/lib/${dylib}" "$(brew --prefix)/lib/${dylib}"
+done
