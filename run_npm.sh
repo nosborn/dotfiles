83101369414c0bc3580528ec65d01d5f603a3fab
Nick Osborn <nosborn@noreply.users.github.com>
2019-11-13 20:05:35 +0800

    Update

--- run_npm.sh
+++ run_npm.sh
@@ -5,7 +5,22 @@ set -o nounset
 
 command -v npm >/dev/null 2>&1 || exit 0
 
-for p in markdownlint-cli@0.18 neovim; do
-  npm list --global "${p}" >/dev/null && continue
-  npm install --global --no-optional --no-package-lock --no-update-notifier --only=production "${p}"
+packages="
+  bash-language-server
+  dockerfile-language-server-nodejs
+  markdownlint-cli@0.18
+  neovim
+  yaml-language-server
+"
+
+for p in ${packages}; do
+  npm list --global "${p}" >/dev/null || {
+    npm install \
+      --global \
+      --no-optional \
+      --no-package-lock \
+      --no-update-notifier \
+      --only=production \
+      "${p}"
+  }
 done
