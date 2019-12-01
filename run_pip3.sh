ff6fa1870fcaa74ed78d391f57c78cf93e0b5867
Nick Osborn <nosborn@users.noreply.github.com>
2019-12-02 07:58:18 +0800

    Update private_dot_Brewfile.tmpl Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -10,5 +10,6 @@ if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
   python3 -mpip install --upgrade --quiet --disable-pip-version-check \
     ansible \
     ansible-lint \
-    datadog
+    datadog \
+    jinja2
 fi
