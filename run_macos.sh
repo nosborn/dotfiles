d7404b61d1d421d3ff48b090ae97fba0d23bea73
Nick Osborn <nosborn@users.noreply.github.com>
2019-12-13 09:13:54 +0800

    Update

--- run_macos.sh
+++ run_macos.sh
@@ -8,14 +8,3 @@ set -o nounset
 if [ -d ~/bin ]; then
   SetFile -a V ~/bin
 fi
-
-_fish_shell="$(brew --prefix)/bin/fish"
-if [ -e "${_fish_shell}" ]; then
-  if ! grep -Fqx "${_fish_shell}" /etc/shells; then
-    echo "${_fish_shell}" | sudo tee -a /etc/shells >/dev/null
-  fi
-  if [ "${SHELL}" != "${_fish_shell}" ]; then
-    echo chpass -s "${_fish_shell}"
-  fi
-fi
-unset _fish_shell
