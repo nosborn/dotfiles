0d5a700fcec526203f1111ecf2ec078f2dfe9a9c
Nick Osborn <nosborn@noreply.users.github.com>
2019-12-20 17:55:43 +0800

    Add private_dot_hammerspoon/init.lua Add private_dot_vim/exact_after/ftplugin/lua.vim Update run_macos.sh

--- run_macos.sh
+++ run_macos.sh
@@ -9,5 +9,5 @@ if [ -d ~/bin ]; then
   SetFile -a V ~/bin
 fi
 
-defaults write -app Hammerspoon HSUploadCrashData -int 0
-defaults write -app Hammerspoon MJShowDockIconKey -int 0
+defaults write -app Hammerspoon HSUploadCrashData 0
+defaults write -app Hammerspoon MJShowDockIconKey 0
