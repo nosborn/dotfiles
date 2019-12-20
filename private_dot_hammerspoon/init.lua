06f9ea33d08456bb93b420e0fca12325226a5a5b
Nick Osborn <nosborn@noreply.users.github.com>
2019-12-20 17:56:18 +0800

    Update private_dot_Brewfile.tmpl Remove private_dot_hammerspoon/init.lua Update run_macos.sh

--- private_dot_hammerspoon/init.lua
+++
@@ -1,9 +0,0 @@
-function battery_watcher()
-  if hs.battery.percentage() < 100 or hs.battery.powerSource == 'AC Power' then
-    os.execute('defaults write com.apple.menuextra.battery ShowPercent YES')
-  else
-    os.execute('defaults write com.apple.menuextra.battery ShowPercent NO')
-  end
-end
-
-hs.battery.watcher.new(battery_watcher):start()
