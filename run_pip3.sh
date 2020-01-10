afdae55e2844098f315d93090803598f6394fda1
Nick Osborn <nosborn@users.noreply.github.com>
2020-01-10 10:32:50 +0800

    Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -10,6 +10,7 @@ if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
   python3 -mpip install --upgrade --quiet --disable-pip-version-check \
     ansible \
     ansible-lint \
+    azure-cli \
     datadog \
     jinja2
 fi
