b87ee47f563814cd5fc2efe7a2cc1f21cf052fe4
Nick Osborn <nosborn@users.noreply.github.com>
2020-02-24 08:38:44 +0800

    Update

--- run_pip3.sh
+++ run_pip3.sh
@@ -11,7 +11,6 @@ if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
   python3 -mpip install --upgrade --quiet --disable-pip-version-check \
     ansible \
     ansible-lint \
-    azure \
     datadog \
     jinja2 \
     molecule\[docker\] \
