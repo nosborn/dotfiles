fdee727af7c745fbd50e17f5e8cfcb66e29ba9db
Nick Osborn <nosborn@users.noreply.github.com>
2020-03-05 09:58:41 +0800

    Update

--- run_macos.sh
+++ run_macos.sh
@@ -19,10 +19,15 @@ defaults write -app BitBar pluginsDirectory "${HOME}/.local/libexec/bitbar"
 defaults write -app Rectangle launchOnLogin -bool true
 # defaults write -app Rectangle subsequentExecutionMode -int 2
 
+defaults write com.JohnCoates.Aerial SUEnableAutomaticChecks -bool false
+defaults write com.JohnCoates.Aerial SUSendProfileInfo -bool false
+
 readonly username=$(id -un)
-sudo tee /etc/sudoers.d/${username}-openconnect >/dev/null <<EOF
+test -e /etc/sudoers.d/${username}-openconnect || {
+  sudo tee /etc/sudoers.d/${username}-openconnect >/dev/null <<EOF
 ${username} ALL=(ALL) NOPASSWD: /usr/bin/killall openconnect
 ${username} ALL=(ALL) NOPASSWD: /usr/local/bin/openconnect
 EOF
-sudo chown root:wheel /etc/sudoers.d/${username}-openconnect
-sudo chmod 0440 /etc/sudoers.d/${username}-openconnect
+  sudo chown root:wheel /etc/sudoers.d/${username}-openconnect
+  sudo chmod 0440 /etc/sudoers.d/${username}-openconnect
+}
