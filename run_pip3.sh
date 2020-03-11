1099e9fa5b7b172698401f5c213f9594f0191a74
Nick Osborn <nosborn@noreply.users.github.com>
2020-03-12 07:20:11 +0800

    Update dot_Brewfile.tmpl Update private_dot_zshrc.tmpl Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -4,14 +4,14 @@ set -o errexit
 set -o nounset
 
 python3 -mpip install --upgrade --quiet --disable-pip-version-check \
-  vim-vint
+  'vim-vint'
 
 if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
   python3 -mpip install --upgrade --quiet --disable-pip-version-check \
-    ansible \
-    ansible-lint \
-    datadog \
-    jinja2 \
-    molecule\[docker\] \
-    packaging
+    'ansible[azure]' \
+    'ansible-lint' \
+    'datadog' \
+    'jinja2' \
+    'molecule[docker]' \
+    'packaging'
 fi
