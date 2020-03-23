f1ce3a7ade3be6006bf7105f852c101bb003655e
Nick Osborn <nosborn@users.noreply.github.com>
2020-03-23 11:28:00 +0800

    Update dot_Brewfile.tmpl Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -4,15 +4,20 @@ set -o errexit
 set -o nounset
 
 python3 -mpip install --upgrade --quiet --disable-pip-version-check \
+  'pynvim' \
   'vim-vint'
 
-# if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
-#   python3 -mpip install --upgrade --quiet --disable-pip-version-check \
-#     'ansible[azure]' \
-#     'ansible-lint' \
-#     'azure-cli' \
-#     'datadog' \
-#     'jinja2' \
-#     'molecule[docker]' \
-#     'packaging'
-# fi
+if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
+  # python3 -mpip install --upgrade --quiet --disable-pip-version-check \
+  #   'ansible[azure]' \
+  #   'ansible-lint' \
+  #   'azure-cli' \
+  #   'datadog' \
+  #   'jinja2' \
+  #   'molecule[docker]' \
+  #   'packaging'
+
+  "$(brew --prefix ansible)/libexec/bin/python3" -mpip install \
+    --upgrade --quiet --disable-pip-version-check \
+    'ansible[azure]'
+fi
