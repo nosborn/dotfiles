2225085efa748ad0b3849c1de2c3827fbbb9ccc6
Nick Osborn <nosborn@users.noreply.github.com>
2020-05-14 21:43:14 +0800

    Update dot_Brewfile.tmpl Update dot_hammerspoon/init.lua Update run_brew.sh Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -8,8 +8,5 @@ python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
 
 if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
   python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
-    'ansible[azure]' \
-    'datadog-checks-dev[cli]' \
-    'molecule[docker]' \
-    'packaging'
+    'datadog-checks-dev[cli]'
 fi
