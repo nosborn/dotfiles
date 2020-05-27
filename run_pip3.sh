8bac5001b8b20e302e21ab84e8f49e650a749360
Nick Osborn <nosborn@users.noreply.github.com>
2020-05-27 15:08:30 +0800

    Update dot_Brewfile.tmpl Update run_brew.sh Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -4,9 +4,13 @@ set -o errexit
 set -o nounset
 
 python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
+  'jinja2' \
   'vim-vint'
 
 if [ "$(hostname -s)" = "19SGD-MAC11" ]; then
   python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
-    'datadog-checks-dev[cli]'
+    'ansible' \
+    'ansible-lint' \
+    'datadog-checks-dev[cli]' \
+    'molecule'
 fi
