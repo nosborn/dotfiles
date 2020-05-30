94a4178a665721f74c43c2027a31886a41f592a6
Nick Osborn <nosborn@noreply.users.github.com>
2020-05-30 09:23:41 +0800

    Update .chezmoiremove Remove dot_emacs.d/early-init.el Remove dot_emacs.d/exact_lisp/init-elpa.el Remove dot_emacs.d/exact_lisp/init-flycheck.el Remove dot_emacs.d/init.el Remove z

--- dot_emacs.d/exact_lisp/init-flycheck.el
+++
@@ -1,8 +0,0 @@
-(require-package 'flycheck)
-(add-hook 'after-init-hook 'global-flycheck-mode)
-(setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
-
-(require-package 'flycheck-color-mode-line)
-(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
-
-(provide 'init-flycheck)
