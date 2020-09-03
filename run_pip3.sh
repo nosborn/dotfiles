7fbf1c7c1e77fbdaa8a17407169937e2d3bf7aea
Nick Osborn <nosborn@users.noreply.github.com>
2020-09-03 08:22:09 +0800

    Update dot_Brewfile.tmpl Update dot_zshrc.tmpl Update run_brew.sh Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -12,7 +12,7 @@ python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
 case "$(hostname -s)" in
   19SGD-*)
     python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
-      'ansible[azure]>=2.9.0,<2.10.0' \
+      'ansible[azure]==2.9.11' \
       'ansible-lint' \
       'datadog' \
       'datadog-checks-dev[cli]' \
