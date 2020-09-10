af6f661545b3f3977e9683a02cf58233be88426a
Nick Osborn <nosborn@users.noreply.github.com>
2020-09-10 14:15:49 +0800

    Update dot_Brewfile.tmpl Update dot_config/pip/pip.conf Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -3,24 +3,19 @@
 set -o errexit
 set -o nounset
 
+unset PYTHONPATH # just in case
 PATH="$(python3 -msite --user-base)/bin:${PATH}"
 
 python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
-  pip
-
-python3 -mpip install --user --upgrade --quiet \
   'vim-vint'
 
 case "$(hostname -s)" in
   19SGD-*)
-    python3 -mpip install --user --upgrade --quiet \
-      datadog \
-      msrest \
-      msrestazure
-    # 'ansible[azure]==2.9.11' \
-    # 'ansible-lint' \
-    # 'datadog' \
-    # 'molecule[docker]'
+    python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
+      'datadog' \
+      'molecule[docker]'
+    #  'ansible[azure]==2.9.11' \
+    #  'ansible-lint' \
     # # 'datadog-checks-dev[cli]' \
     ;;
 esac
