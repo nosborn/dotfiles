7b68950612ca54c94a9a93aea6087af8dad03139
Nick Osborn <nosborn@users.noreply.github.com>
2020-10-14 10:35:04 +0800

    Update private_dot_zsh/lib/rprompt.zsh

--- private_dot_zsh/lib/rprompt.zsh
+++ private_dot_zsh/lib/rprompt.zsh
@@ -1,16 +1,3 @@
-# setopt prompt_subst
-
-# function _rprompt_info() {
-#   if [ -e $(brew --prefix zsh-git-prompt)/zshrc.sh ]; then
-#     git_super_status
-#   else
-#     vcs_info
-#     printf '%s' "${vcs_info_msg_0_}"
-#   fi
-# }
-
-# RPROMPT='$(_rprompt_info)'
-
 autoload -Uz vcs_info
 
 precmd_vcs_info() {
@@ -20,6 +7,6 @@ precmd_functions+=(precmd_vcs_info)
 
 setopt prompt_subst
 RPROMPT='${vcs_info_msg_0_}'
-zstyle ':vcs_info:git:*' formats '%F{3}%b'
+zstyle ':vcs_info:git:*' formats '%F{3}[%b]%f'
 
 # vim: ft=zsh
