236784201920e425dea54c651c6b2a4aaf67dd20
Nick Osborn <nosborn@users.noreply.github.com>
2020-10-15 10:32:49 +0800

    Add dot_zsh/exact_lib/terraform.zsh Remove private_dot_zsh/lib/git.zsh Rename private_dot_zsh/lib/abbreviated-path.zsh to dot_zsh/exact_lib/abbreviated-path.zsh Rename private_dot_zsh/lib/completion.zsh to dot_zsh/exact_lib/completion.zsh Rename private_dot_zsh/lib/grep.zsh to dot_zsh/exact_lib/grep.zsh Rename private_dot_zsh/lib/rprompt.zsh to dot_zsh/exact_lib/rprompt.zsh

--- dot_zsh/exact_lib/rprompt.zsh
+++ dot_zsh/exact_lib/rprompt.zsh
@@ -7,6 +7,7 @@ precmd_functions+=(precmd_vcs_info)
 
 setopt prompt_subst
 RPROMPT='${vcs_info_msg_0_}'
-zstyle ':vcs_info:git:*' formats '%F{3}[%b]%f'
+zstyle ':vcs_info:git:*' actionformats '%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f'
+zstyle ':vcs_info:git:*' formats '%F{5}[%F{2}%b%F{5}]%f'
 
 # vim: ft=zsh
