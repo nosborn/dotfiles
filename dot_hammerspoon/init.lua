fa533b332cc30430f5cf48b792ef38bc46691f9b
Nick Osborn <nosborn@noreply.users.github.com>
2020-10-20 06:58:24 +0800

    Update .chezmoiremove Update dot_Brewfile.tmpl Update dot_config/git/attributes Remove dot_hammerspoon/init.lua Remove dot_local/etc/executable_vpnc-script Remove dot_local/share/git-core/contrib/diff-highlight/.chezmoiignore Remove dot_local/share/git-core/contrib/diff-highlight/DiffHighlight.pm Remove dot_local/share/git-core/contrib/diff-highlight/README Remove dot_local/share/git-core/contrib/diff-highlight/executable_diff-highlight

--- dot_hammerspoon/init.lua
+++
@@ -1,6 +0,0 @@
-keyDown = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
-  local characters = event:getCharacters()
-  local keyCode = event:getKeyCode()
-  print("down", characters, keyCode)
-end)
-keyDown:start()
