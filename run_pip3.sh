3abdae9d3594cb56700852850f4828ad261e7bae
Nick Osborn <nosborn@users.noreply.github.com>
2020-12-11 11:57:27 +0800

    Update dot_Brewfile.tmpl Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -13,11 +13,11 @@ python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
 case "$(hostname -s)" in
   19SGD-*)
     python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
-      'azure-cli' \
+      'ansible>=2.10,<2.11' \
+      'ansible-lint>4.3' \
+      'azure-cli>=2.16' \
       'datadog' \
       'datadog-checks-dev[cli]' \
-      'molecule[docker]' \
-      'msrestazure' \
-      'packaging'
+      'molecule[docker]>=3.2'
     ;;
 esac
