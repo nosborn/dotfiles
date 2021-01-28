7c02391e20e731be02f637d2d1b275589ac9f232
Nick Osborn <nosborn@users.noreply.github.com>
2021-01-28 15:26:05 +0800

    Update dot_config/starship.toml Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -13,12 +13,12 @@ python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
 case "$(hostname -s)" in
   19SGD-MAC11 | 19SGD-MAC11-*)
     python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
-      'ansible>=2.10,<2.11' \
-      'ansible-lint>4.3' \
-      'azure-cli>=2.16' \
+      'ansible>=2.10.6,<2.11' \
+      'ansible-lint>=4.3.7' \
+      'azure-cli>=2.18' \
       'datadog' \
       'datadog-checks-dev[cli]' \
-      'molecule[docker]>=3.2' \
+      'molecule[docker]>=3.2.2' \
       'mssql-cli'
     ;;
 esac
