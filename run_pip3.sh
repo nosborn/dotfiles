235aebbeb86ed999a94dd0f3fec0992815911f0d
Nick Osborn <nosborn@noreply.users.github.com>
2021-02-11 20:45:07 +0800

    Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -8,16 +8,17 @@ PATH="$(python3 -msite --user-base)/bin:${PATH}"
 
 python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
   'argcomplete' \
+  'netaddr' \
   'vim-vint'
 
 if [ "$(chezmoi data | jq -r .where)" = work ]; then
   python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
-    'ansible>=2.10.5,<2.11' \
-    'ansible-lint>=4.3.7' \
-    'azure-cli>=2.18' \
+    'ansible>=2.10.7,<2.11' \
+    'ansible-lint>=5.0.0' \
+    'azure-cli>=2.19' \
     'datadog' \
     'datadog-checks-dev[cli]' \
-    'molecule[docker]>=3.2.2' \
+    'molecule[docker]>=3.2.3' \
     'mssql-cli' \
     'netaddr' \
     'openconnect-sso'
