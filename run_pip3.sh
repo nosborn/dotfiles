7a0f06ad4ca730fafe884ac07a7d84ca7de8b42d
Nick Osborn <nosborn@users.noreply.github.com>
2021-03-19 10:32:53 +0800

    Update run_pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -6,18 +6,18 @@ set -o nounset
 unset PYTHONPATH # just in case
 PATH="$(python3 -msite --user-base)/bin:${PATH}"
 
-python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
+python3 -mpip install --user --upgrade --quiet --no-input --disable-pip-version-check \
   'argcomplete' \
   'netaddr' \
   'vim-vint'
 
 if [ "$(chezmoi data | jq -r .where)" = work ]; then
-  python3 -mpip install --user --upgrade --quiet --disable-pip-version-check \
-    'ansible>=2.10.7,<2.11' \
-    'ansible-lint>=5.0.0' \
-    'azure-cli>=2.19' \
+  python3 -mpip install --user --upgrade --quiet --no-input --disable-pip-version-check \
+    'ansible>=3.0,<3.1' \
+    "ansible-lint==$(brew info ansible-lint --json | jq -r '.[].versions.stable')" \
+    "azure-cli==$(brew info azure-cli --json | jq -r '.[].versions.stable')" \
     'datadog' \
     'datadog-checks-dev[cli]' \
-    'molecule[docker]>=3.2.3' \
+    "molecule[docker]==$(brew info molecule --json | jq -r '.[].versions.stable')" \
     'mssql-cli'
 fi
