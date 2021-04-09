140a9bc43c4a336042ec70955b555cc235a1b741
Nick Osborn <nosborn@users.noreply.github.com>
2021-04-09 10:49:43 +0800

    Update dot_zsh/exact_lib/terraform.zsh

--- dot_zsh/exact_lib/terraform.zsh
+++ dot_zsh/exact_lib/terraform.zsh
@@ -2,6 +2,6 @@ if (( ${+commands[terraform]} )); then
   autoload -U +X bashcompinit && bashcompinit
   complete -o nospace -C "${commands[terraform]}" terraform
 
-  alias tfdiff='command diff -dNry -W${COLUMNS} -x .terraform -x \*.tfplan -x \*.rules.tf -x \*.tfstate -x \*.tfstate.\* -x PLAN'
+  alias tfdiff='command diff -dNry -W${COLUMNS} -x .terraform -x .terraform.lock.hcl -x \*.tfplan -x \*.tfstate -x \*.tfstate.\* -x PLAN'
   alias tfdiffs='tfdiff --suppress-common-lines'
 fi
