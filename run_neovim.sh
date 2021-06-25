b45843fae701a2b48e2faedb566b19280433387c
Nick Osborn <nosborn@users.noreply.github.com>
2021-06-25 15:31:55 +0800

    Add dot_config/nvim/exact_after/ftplugin/bindzone.vim Add dot_config/nvim/exact_after/ftplugin/c.vim Add dot_config/nvim/exact_after/ftplugin/gitcommit.vim Add dot_config/nvim/exact_after/ftplugin/groovy.vim Add dot_config/nvim/exact_after/ftplugin/hocon.vim Add dot_config/nvim/exact_after/ftplugin/javascript.vim Add dot_config/nvim/exact_after/ftplugin/json.vim Add dot_config/nvim/exact_after/ftplugin/lua.vim Add dot_config/nvim/exact_after/ftplugin/make.vim Add dot_config/nvim/exact_after/ftplugin/markdown.vim.tmpl Add dot_config/nvim/exact_after/ftplugin/named.vim Add dot_config/nvim/exact_after/ftplugin/perl.vim Add dot_config/nvim/exact_after/ftplugin/python.vim Add dot_config/nvim/exact_after/ftplugin/sh.vim Add dot_config/nvim/exact_after/ftplugin/sshconfig.vim Add dot_config/nvim/exact_after/ftplugin/terraform.vim Add dot_config/nvim/exact_after/ftplugin/text.vim Add dot_config/nvim/exact_after/ftplugin/toml.vim Add dot_config/nvim/exact_after/ftplugin/vim.vim Add dot_config/nvim/exact_after/ftplugin/xml.vim Add dot_config/nvim/exact_after/ftplugin/yaml.vim Add dot_config/nvim/exact_after/ftplugin/zsh.vim Add dot_config/nvim/exact_after/syntax/terraform.vim Add dot_config/nvim/exact_autoload/lightline/colorscheme/toast.vim Add dot_config/nvim/exact_autoload/lightline/wordcount.vim Add dot_config/nvim/filetype.vim Remove dot_config/nvim/init.lua Add dot_config/nvim/init.vim.tmpl Update run_neovim.sh

--- run_neovim.sh
+++ run_neovim.sh
@@ -3,53 +3,49 @@
 set -o errexit
 set -o nounset
 
-# pack() {
-#   [ -d "${HOME}/.vim/pack/${1%/*}/start/${1#*/}/.git" ] || {
-#     git clone --depth 1 --single-branch -- \
-#       "https://github.com/${1}.git" \
-#       "${HOME}/.vim/pack/${1%/*}/start/${1#*/}"
-#   }
-#   (
-#     cd "${HOME}/.vim/pack/${1%/*}/start/${1#*/}" || exit
-#     git checkout -q master
-#     git pull --ff-only --no-verify
-#     [ -z "${2:-}" ] || git checkout "$2"
-#   )
-# }
+pack() {
+  [ -d "${HOME}/.local/share/nvim/site/pack/${1%/*}/start/${1#*/}/.git" ] || {
+    git clone --depth 1 --single-branch -- \
+      "https://github.com/${1}.git" \
+      "${HOME}/.local/share/nvim/site/pack/${1%/*}/start/${1#*/}"
+  }
+  (
+    cd "${HOME}/.local/share/nvim/site/pack/${1%/*}/start/${1#*/}" || exit
+    git checkout -q master
+    git pull --ff-only --no-verify
+    [ -z "${2:-}" ] || git checkout "$2"
+  )
+}
 
-# # pack JamshedVesuna/vim-markdown-preview
-# pack Xuyuanp/nerdtree-git-plugin
-# pack airblade/vim-gitgutter
-# pack cespare/vim-toml
-# pack chr4/nginx.vim
-# pack dag/vim-fish
-# pack dense-analysis/ale
-# pack digitaltoad/vim-pug
-# pack direnv/direnv.vim
-# pack fatih/vim-go
-# pack godlygeek/tabular
-# pack hashivim/vim-terraform
-# pack itchyny/lightline.vim
-# # pack juliosueiras/vim-terraform-completion
-# pack junegunn/fzf.vim
-# pack maralla/vim-toml-enhance
-# pack maximbaz/lightline-ale
-# pack morhetz/gruvbox
-# pack pangloss/vim-javascript
-# pack pearofducks/ansible-vim
-# pack preservim/nerdtree
-# pack satabin/hocon-vim
-# pack thaerkh/vim-indentguides
-# pack tpope/vim-commentary
-# pack tpope/vim-endwise
-# pack tpope/vim-fugitive
-# # pack tpope/vim-markdown
-# pack tpope/vim-vinegar
-
-if [ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim/.git ]; then
-  git clone https://github.com/wbthomason/packer.nvim \
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-fi
+# pack JamshedVesuna/vim-markdown-preview
+pack Xuyuanp/nerdtree-git-plugin
+pack airblade/vim-gitgutter
+pack cespare/vim-toml
+pack chr4/nginx.vim
+pack dag/vim-fish
+pack dense-analysis/ale
+pack digitaltoad/vim-pug
+pack direnv/direnv.vim
+pack fatih/vim-go
+pack godlygeek/tabular
+pack hashivim/vim-terraform
+pack itchyny/lightline.vim
+# pack juliosueiras/vim-terraform-completion
+pack junegunn/fzf.vim
+pack maralla/vim-toml-enhance
+pack maximbaz/lightline-ale
+pack morhetz/gruvbox
+pack nvim-treesitter/nvim-treesitter
+pack pangloss/vim-javascript
+pack pearofducks/ansible-vim
+pack preservim/nerdtree
+pack satabin/hocon-vim
+pack thaerkh/vim-indentguides
+pack tpope/vim-commentary
+pack tpope/vim-endwise
+pack tpope/vim-fugitive
+# pack tpope/vim-markdown
+pack tpope/vim-vinegar
 
 "$(brew --prefix)/bin/nvim" -n -u NONE --headless \
   +'helptags ALL' \
@@ -60,3 +56,7 @@ for add in ~/.config/nvim/spell/*.add; do
     "+mkspell! ${add}" \
     '+quit'
 done
+
+# "$(brew --prefix)/bin/nvim" -n -u NONE --headless \
+#   +'TSUpdate' \
+#   +'quit'
