13aa0062c9de595311221a18f035d95c8b226f19
Nick Osborn <nosborn@users.noreply.github.com>
2021-07-05 07:01:36 +0800

    Update run_neovim.sh Update run_vim.sh

--- run_vim.sh
+++ run_vim.sh
@@ -18,7 +18,7 @@ pack() {
 }
 
 # pack JamshedVesuna/vim-markdown-preview
-pack PProvost/ps1
+pack PProvost/vim-ps1
 pack Xuyuanp/nerdtree-git-plugin
 pack airblade/vim-gitgutter
 pack cespare/vim-toml
