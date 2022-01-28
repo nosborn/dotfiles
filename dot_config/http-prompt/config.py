c6d88edbe11da394d3d5c247b2ac971151659f93
Nick Osborn <nosborn@users.noreply.github.com>
2022-01-28 13:58:56 +0800

    Update dot_config/http-prompt/config.py

--- dot_config/http-prompt/config.py
+++ dot_config/http-prompt/config.py
@@ -3,7 +3,7 @@
 # fruity, igor, lovelace, manni, monokai, murphy, native, paraiso-dark,
 # paraiso-light, pastie, perldoc, rrt, solarized, tango, trac, vim, vs, xcode.
 # Preview themes at http://http-prompt.com/themes
-command_style = 'default'
+command_style = "default"
 
 # Highlighting style for HTTPie's output. Available values are the same as
 # command_style. Set this to None to use HTTPie's default style, which you
@@ -12,13 +12,13 @@ output_style = None
 
 # The tool used to paginate output. Available values: 'less' and 'more'.
 # Note that 'more' does not support ANSI colors.
-pager = 'less'
+pager = "less"
 
 # What to do when a response has a 'Set-Cookie' header? Available values:
 # 'auto': set the cookie automatically and silently
 # 'ask': ask the user if they want to set the cookie
 # 'off': do nothing with the 'Set-Cookie' header
-set_cookies = 'auto'
+set_cookies = "auto"
 
 # Enable Vi editor mode? Available values: True / False.
 # When Vi mode is enabled, you use Vi-like keybindings to edit your commands.
