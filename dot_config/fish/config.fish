26bded43c85f030c51777572695f41128b74dcea
Nick Osborn <nosborn@users.noreply.github.com>
2022-03-15 06:14:20 +0800

    Update dot_config/fish/config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,5 +1,8 @@
 set --global fish_greeting
 
+fish_add_path --global --prepend "$HOME/.local/bin"
+fish_add_path --global --prepend "$HOME/bin"
+
 if type --quiet direnv
     direnv hook fish | source
 end
