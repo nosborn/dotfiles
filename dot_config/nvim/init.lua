e2256993c612909abf84119f692f2cd994ea6fc6
Nick Osborn <nosborn@users.noreply.github.com>
2022-05-21 08:40:08 +0800

    Update .chezmoiremove Add dot_config/nvim/exact_after/filetype.vim Update dot_config/nvim/filetype.vim Update dot_config/nvim/init.lua Update run_neovim.sh

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -186,3 +186,18 @@ vim.cmd([[
   sign define DiagnosticSignInfo text=Ⓘ texthl=DiagnosticSignInfo linehl= numhl=
   sign define DiagnosticSignWarn text=Ⓦ texthl=DiagnosticSignWarn linehl= numhl=
 ]])
+
+vim.cmd([[
+  function HelmSyntax()
+    set filetype=yaml
+    unlet b:current_syntax
+    syn include @yamlGoTextTmpl syntax/gotexttmpl.vim
+    let b:current_syntax = "yaml"
+    syn region goTextTmpl start=/{{/ end=/}}/ contains=@gotplLiteral,gotplControl,gotplFunctions,gotplVariable,goTplIdentifier containedin=ALLBUT,goTextTmpl keepend
+    hi def link goTextTmpl PreProc
+  endfunction
+  augroup helm_syntax
+    autocmd!
+    autocmd BufRead,BufNewFile */templates/*.yaml,*/templates/*.tpl call HelmSyntax()
+  augroup END
+]])
