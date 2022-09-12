774f7804407699aa8f5aceea62ed9fce77a0c632
Nick Osborn <nosborn@users.noreply.github.com>
2022-09-13 06:40:19 +0800

    Update dot_config/nvim/exact_after/exact_plugin/telescope.lua

--- dot_config/nvim/exact_after/exact_plugin/telescope.lua
+++ dot_config/nvim/exact_after/exact_plugin/telescope.lua
@@ -1,1 +1,14 @@
-require("telescope").setup()
+require("telescope").setup({
+  defaults = {
+    prompt_prefix = "❯ ",
+    selection_prefix = "❯",
+  },
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    },
+    live_grep = {
+      theme = "dropdown",
+    },
+  },
+})
