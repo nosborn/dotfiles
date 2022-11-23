6e19db2e3b16a61e51b87cd55d0a2662b86647bb
Nick Osborn <nosborn@users.noreply.github.com>
2022-11-23 21:44:28 +0800

    Update .Brewfile Update macos.sh

--- run_macos.sh
+++ run_macos.sh
@@ -8,7 +8,6 @@ set -o nounset
 if [ -d /Applications/Rectangle.app ]; then
   defaults write -app Rectangle SUEnableAutomaticChecks -bool false
   defaults write -app Rectangle SUHasLaunchedBefore -bool true
-  # defaults write -app Rectangle SUSendProfileInfo -bool false
   defaults write -app Rectangle alternateDefaultShortcuts -bool true
   defaults write -app Rectangle disabledApps -string '["com.apple.finder"]'
   defaults write -app Rectangle launchOnLogin -bool true
@@ -16,8 +15,11 @@ if [ -d /Applications/Rectangle.app ]; then
 fi
 
 find "$(brew --prefix)/Caskroom" -name '*.app' -type l | while read -r link; do
+  case "${link}" in
+    */Tunnelblick.app) continue ;;
+  esac
   if xattr "${link}" | grep -Fqx com.apple.quarantine; then
-    xattr -d -v com.apple.quarantine "${link}" || :
+    xattr -d -v com.apple.quarantine "${link}"
   fi
 done
 
