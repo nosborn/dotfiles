2dbd530fae6d6ebe03fe1755e0a4f8c0f8e9cb3d
Nick Osborn <nosborn@users.noreply.github.com>
2023-02-18 08:22:29 +0800

    Add .chezmoiexternal.toml Remove .chezmoiexternal.yaml Update .Brewfile Update pip3.sh

--- run_pip3.sh
+++ run_pip3.sh
@@ -3,7 +3,7 @@
 set -o errexit
 set -o nounset
 
-"$(brew --prefix python3)/bin/python3" -m pip install \
+"$(brew --prefix)/bin/python3" -m pip install \
   --disable-pip-version-check \
   --no-deps \
   --requirement "$(chezmoi source-path)/requirements.txt" \
