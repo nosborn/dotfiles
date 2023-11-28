fd4dfc7d01902155e50b53c768580d3fbd462b41
Nick Osborn <nosborn@users.noreply.github.com>
2023-11-29 04:58:51 +0800

    Update .Brewfile Update .config/nvim/after/ftplugin/terraform.vim Update .config/nvim/after/plugin/gitsigns.lua Update .config/nvim/after/plugin/null-ls.lua

--- dot_config/nvim/exact_after/exact_plugin/gitsigns.lua
+++ dot_config/nvim/exact_after/exact_plugin/gitsigns.lua
@@ -3,6 +3,7 @@ require("gitsigns").setup({
   current_line_blame_opts = {
     delay = vim.o.updatetime,
     ignore_whitespace = true,
+    virt_text_pos = "right_align",
   },
   current_line_blame_formatter_opts = {
     relative_time = true,
--- dot_config/nvim/exact_after/exact_plugin/null-ls.lua
+++ dot_config/nvim/exact_after/exact_plugin/null-ls.lua
@@ -11,7 +11,7 @@ null_ls.setup({
           vim.lsp.buf.format({
             bufnr = bufnr,
             filter = function(client)
-              return client.name == "none-ls"
+              return client.name == "null-ls"
             end,
           })
         end,
@@ -54,5 +54,26 @@ null_ls.setup({
     null_ls.builtins.diagnostics.vint,
     null_ls.builtins.diagnostics.yamllint,
     null_ls.builtins.diagnostics.zsh,
+    -- Formatting
+    null_ls.builtins.formatting.black,
+    --null_ls.builtins.formatting.eslint,
+    --null_ls.builtins.formatting.ktlint,
+    --null_ls.builtins.formatting.packer,
+    null_ls.builtins.formatting.prettier.with({
+      extra_filetypes = { "toml" },
+    }),
+    null_ls.builtins.formatting.rubocop,
+    null_ls.builtins.formatting.shfmt.with({
+      extra_args = { "-i", "2", "-ci" },
+    }),
+    null_ls.builtins.formatting.stylua,
+    --null_ls.builtins.formatting.terrafmt
+    null_ls.builtins.formatting.terraform_fmt.with({
+      extra_filetypes = { "hcl" },
+    }),
+    null_ls.builtins.formatting.xmllint.with({
+      extra_args = { "--nonet" },
+    }),
+    -- Hover
   },
 })
