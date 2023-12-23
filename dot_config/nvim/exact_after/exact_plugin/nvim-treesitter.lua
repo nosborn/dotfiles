55cb3bcd8d7bb51b0789a24167550bf9bc5ac54b
Nick Osborn <nosborn@users.noreply.github.com>
2023-12-23 08:33:07 +0800

    Update .chezmoiexternal.toml Update .chezmoiremove Update README.md Update .Brewfile Add .config/nvim/after/plugin/lspconfig.lua Add .config/nvim/after/plugin/noice.lua Update .config/nvim/after/plugin/nvim-treesitter.lua Change attributes of LICENSE.md

--- dot_config/nvim/exact_after/exact_plugin/nvim-treesitter.lua
+++ dot_config/nvim/exact_after/exact_plugin/nvim-treesitter.lua
@@ -58,3 +58,15 @@ require("nvim-treesitter.configs").setup({
     enable = true,
   },
 })
+
+require("nvim-treesitter.parsers").get_parser_configs().river = {
+  install_info = {
+    branch = "main",
+    files = {
+      "src/parser.c",
+    },
+    generate_requires_npm = false,
+    requires_generate_from_grammar = false,
+    url = "https://github.com/grafana/tree-sitter-river.git",
+  },
+}
