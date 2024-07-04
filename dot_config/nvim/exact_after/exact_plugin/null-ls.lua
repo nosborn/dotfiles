e9c9fdfaf68cc4c90a0ec4fd81958ffd33b409c8
Nick Osborn <nosborn@users.noreply.github.com>
2024-07-05 06:11:28 +0800

    Update .Brewfile Update .config/nvim/after/plugin/null-ls.lua

--- dot_config/nvim/exact_after/exact_plugin/null-ls.lua
+++ dot_config/nvim/exact_after/exact_plugin/null-ls.lua
@@ -15,7 +15,7 @@ null_ls.setup({
     null_ls.builtins.diagnostics.golangci_lint,
     null_ls.builtins.diagnostics.hadolint,
     null_ls.builtins.diagnostics.markdownlint,
-    null_ls.builtins.diagnostics.rubocop,
+    --null_ls.builtins.diagnostics.rubocop,
     --null_ls.builtins.diagnostics.terraform_validate,
     --null_ls.builtins.diagnostics.tfsec, -- DEPRECATED
     --null_ls.builtins.diagnostics.trivy,
@@ -27,7 +27,7 @@ null_ls.setup({
     null_ls.builtins.formatting.prettier.with({
       extra_filetypes = { "toml" },
     }),
-    null_ls.builtins.formatting.rubocop,
+    --null_ls.builtins.formatting.rubocop,
     null_ls.builtins.formatting.shfmt.with({
       extra_args = { "-i", "2", "-ci" },
     }),
