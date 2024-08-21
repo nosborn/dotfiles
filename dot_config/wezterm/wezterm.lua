858584e4d222902fec2b2fc4aaf2b9294036b5f6
Nick Osborn <nosborn@users.noreply.github.com>
2024-08-22 06:25:19 +0800

    Update .chezmoiremove Update .Brewfile Update .config/kitty/kitty.conf Add .config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.color_scheme = "Tomorrow Night"
+config.font = wezterm.font("JetBrains Mono NL")
+config.font_size = 16.0
+config.initial_cols = 88
+config.initial_rows = 44
+
+return config
