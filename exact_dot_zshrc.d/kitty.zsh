b3767347ad9895cddb134a54dd371f5961c224ec
Nick Osborn <nosborn@users.noreply.github.com>
2024-12-31 07:36:44 +0800

    Update .chezmoiexternal.toml Update .chezmoiremove Add .chezmoiscripts/fish.sh Update .Brewfile Remove .config/kitty/kitty.conf Remove .config/kitty/macos-launch-services-cmdline Add .config/fish/config.fish Add .config/fish/completions/.keep Add .config/fish/conf.d/.keep Add .config/fish/functions/.keep Add .config/ghostty/config Add .config/nvim/after/ftplugin/fish.vim Remove .zshrc.d/kitty.zsh

--- exact_dot_zshrc.d/kitty.zsh
+++
@@ -1,6 +0,0 @@
-if test -n "${KITTY_INSTALLATION_DIR}"; then
-  export KITTY_SHELL_INTEGRATION='no-cursor'
-  autoload -Uz -- "${KITTY_INSTALLATION_DIR}/shell-integration/zsh/kitty-integration"
-  kitty-integration
-  unfunction kitty-integration
-fi
