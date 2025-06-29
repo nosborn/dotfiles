832153212415a9047db39d1aecb031ba411e5610
Nick Osborn <nosborn@users.noreply.github.com>
2025-06-29 10:14:19 +0800

    Update .chezmoi.toml Update .chezmoiignore Update .chezmoiremove Update .chezmoiscripts/1_npm.sh Update .gitattributes Add .github/dependabot.yml Update .gitignore Add .gemini/settings.json Remove .gemini/settings.json Add package-lock.json Update package.json

--- .chezmoiscripts/run_after_1_npm.sh
+++ .chezmoiscripts/run_after_1_npm.sh
@@ -6,4 +6,20 @@ set -o nounset
 command -v npm >/dev/null 2>&1 || exit 0
 
 cd "${CHEZMOI_SOURCE_DIR:?}"
-npm install --location=global --omit=dev --prefix="${HOME}/.local"
+
+if [ ! -e package-lock.json ]; then
+  npm install --omit=dev --no-fund --package-lock-only
+fi
+
+INSTALLED="$(npm list --depth=0 --json --location=global --prefix="${HOME}/.local")"
+WANTED="$(jq -r '.dependencies | to_entries | .[] | .key + "@" + .value' package.json)"
+
+for pkg_spec in ${WANTED}; do
+  pkg_name=${pkg_spec%@*}
+  wanted=${pkg_spec##*@}
+  installed=$(echo "${INSTALLED}" | jq -r ".dependencies.\"${pkg_name}\".version // \"\"")
+
+  if [ "${installed}" != "${wanted}" ]; then
+    npm install --location=global --no-fund --omit=dev --prefix="${HOME}/.local" "${pkg_spec}"
+  fi
+done
