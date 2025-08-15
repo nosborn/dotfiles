0b60faf6a4ec1b74d1d42bfaf0b9faf6b8785cdf
Nick Osborn <nosborn@users.noreply.github.com>
2025-08-15 14:19:22 +0800

    Update .chezmoiexternal.yaml Update .Brewfile Update .config/nvim/after/plugin/conform.nvim.lua Update .config/nvim/after/plugin/nvim-lint.lua Remove .config/nvim/lsp/golangci_lint_ls.lua Update .config/nvim/lsp/gopls.lua Update .config/nvim/init.lua Update .zshenv

--- dot_config/nvim/exact_lsp/gopls.lua
+++ dot_config/nvim/exact_lsp/gopls.lua
@@ -1,24 +1,31 @@
 return {
     cmd = { 'gopls' },
     filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+    root_markers = { 'go.mod', 'go.sum' },
     settings = {
         gopls = {
-            analyses = {
-                nilness = true,
-                unusedparams = true,
-                unusedwrite = true,
-            },
-            codelenses = {
-                generate = true,
-                test = true,
-                tidy = true,
-            },
+            -- Build
+            -- Formatting
             gofumpt = true,
+            -- UI
+            --codelenses = {
+            --    generate = true,
+            --    test = true,
+            --    tidy = true,
+            --},
+            -- Completion
+            -- Diagnostic
+            --analyses = {
+            --    --ST1000 = false, -- Incorrect or missing package comment
+            --},
+            --staticcheck = true,
+            -- Documentation
+            -- Inlayhint
             hints = {
                 constantValues = true,
                 parameterNames = true,
             },
-            staticcheck = true,
+            -- Navigation
         },
     },
 }
