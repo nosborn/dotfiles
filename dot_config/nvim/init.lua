dba9fb813b7a92d6041c9bdee337118d9a94d311
Nick Osborn <nosborn@users.noreply.github.com>
2025-10-04 07:03:43 +0800

    Update .Brewfile Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -66,6 +66,39 @@ vim.keymap.set('i', '<C-k>', '<C-o>C')
 vim.keymap.set('c', '<C-a>', '<Home>')
 vim.keymap.set('c', '<C-e>', '<End>')
 
+-- vim.lsp.enable({
+--     'dockerls',
+-- })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('lsp-attach', {}),
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        if client:supports_method('textDocument/implementation') then
+            -- Create a keymap for vim.lsp.buf.implementation ...
+        end
+        -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+        if client:supports_method('textDocument/completion') then
+            -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+            -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+            -- client.server_capabilities.completionProvider.triggerCharacters = chars
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+        end
+        -- Auto-format ("lint") on save.
+        -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+        if
+            not client:supports_method('textDocument/willSaveWaitUntil')
+            and client:supports_method('textDocument/formatting')
+        then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                group = vim.api.nvim_create_augroup('lsp-attach', { clear = false }),
+                buffer = args.buf,
+                callback = function() vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 }) end,
+            })
+        end
+    end,
+})
+
 local map_multistep = require('mini.keymap').map_multistep
 map_multistep('i', '<Tab>', { 'pmenu_next' })
 map_multistep('i', '<S-Tab>', { 'pmenu_prev' })
