15dc40a155598f406aec1cfb9c75fe7d75da3385
Nick Osborn <nosborn@users.noreply.github.com>
2026-01-11 08:49:51 +0000

    Remove .zshrc.d/fzf.zsh Add .zshrc.d/fzf.zsh Remove .zshrc.d/skip-osc-sequence.zsh

--- exact_dot_zshrc.d/fzf.zsh
+++
@@ -1,6 +0,0 @@
-[[ "${PATH}" == */opt/homebrew/opt/fzf/bin* ]] || {
-  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
-}
-
-source /opt/homebrew/opt/fzf/shell/completion.zsh
-source /opt/homebrew/opt/fzf/shell/key-bindings.zsh
