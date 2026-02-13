7c95ab8d24530f20f83a87f2f7a79522f5c1077b
Nick Osborn <nosborn@users.noreply.github.com>
2026-02-13 08:35:50 +0000

    Update .gitignore Update .claude/settings.json Update .config/nvim/after/plugin/conform.nvim.lua Update .config/nvim/lsp/terraform_ls.lua Update .zshenv

--- dot_config/nvim/exact_lsp/terraform_ls.lua
+++ dot_config/nvim/exact_lsp/terraform_ls.lua
@@ -14,10 +14,6 @@ return {
             validateOnSave = true,
         },
     },
-    on_attach = function(client, _bufnr)
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-    end,
     root_markers = {
         '.git',
         '.terraform',
