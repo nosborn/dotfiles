ac02bc706efd89e0965e0b5a43f8bbd9817dcdb5
Nick Osborn <nosborn@users.noreply.github.com>
2026-03-06 04:19:34 +0000

    Update .Brewfile Update .config/nvim/after/plugin/mini.lua

--- dot_config/nvim/exact_after/exact_plugin/mini.lua
+++ dot_config/nvim/exact_after/exact_plugin/mini.lua
@@ -1,3 +1,97 @@
+local nmap_leader = function(suffix, rhs, desc)
+    vim.keymap.set('n', '<Leader>' .. suffix, rhs, { desc = desc })
+end
+local xmap_leader = function(suffix, rhs, desc)
+    vim.keymap.set('x', '<Leader>' .. suffix, rhs, { desc = desc })
+end
+
+-- b is for 'Buffer'
+local new_scratch_buffer = function()
+    vim.api.nvim_win_set_buf(0, vim.api.nvim_create_buf(true, true))
+end
+nmap_leader('ba', '<Cmd>b#<CR>', 'Alternate')
+nmap_leader('bd', '<Cmd>lua MiniBufremove.delete()<CR>', 'Delete')
+nmap_leader('bD', '<Cmd>lua MiniBufremove.delete(0, true)<CR>', 'Delete!')
+nmap_leader('bs', new_scratch_buffer, 'Scratch')
+nmap_leader('bw', '<Cmd>lua MiniBufremove.wipeout()<CR>', 'Wipeout')
+nmap_leader('bW', '<Cmd>lua MiniBufremove.wipeout(0, true)<CR>', 'Wipeout!')
+
+-- e is for 'Explore' and 'Edit'
+
+-- f is for 'Fuzzy Find'
+local pick_added_hunks_buf = '<Cmd>Pick git_hunks path="%" scope="staged"<CR>'
+local pick_workspace_symbols_live = '<Cmd>Pick lsp scope="workspace_symbol_live"<CR>'
+nmap_leader('f/', '<Cmd>Pick history scope="/"<CR>', '"/" history')
+nmap_leader('f:', '<Cmd>Pick history scope=":"<CR>', '":" history')
+nmap_leader('fa', '<Cmd>Pick git_hunks scope="staged"<CR>', 'Added hunks (all)')
+nmap_leader('fA', pick_added_hunks_buf, 'Added hunks (buf)')
+nmap_leader('fb', '<Cmd>Pick buffers<CR>', 'Buffers')
+nmap_leader('fc', '<Cmd>Pick git_commits<CR>', 'Commits (all)')
+nmap_leader('fC', '<Cmd>Pick git_commits path="%"<CR>', 'Commits (buf)')
+nmap_leader('fd', '<Cmd>Pick diagnostic scope="all"<CR>', 'Diagnostic workspace')
+nmap_leader('fD', '<Cmd>Pick diagnostic scope="current"<CR>', 'Diagnostic buffer')
+nmap_leader('ff', '<Cmd>Pick files<CR>', 'Files')
+nmap_leader('fg', '<Cmd>Pick grep_live<CR>', 'Grep live')
+nmap_leader('fG', '<Cmd>Pick grep pattern="<cword>"<CR>', 'Grep current word')
+nmap_leader('fh', '<Cmd>Pick help<CR>', 'Help tags')
+nmap_leader('fH', '<Cmd>Pick hl_groups<CR>', 'Highlight groups')
+nmap_leader('fl', '<Cmd>Pick buf_lines scope="all"<CR>', 'Lines (all)')
+nmap_leader('fL', '<Cmd>Pick buf_lines scope="current"<CR>', 'Lines (buf)')
+nmap_leader('fm', '<Cmd>Pick git_hunks<CR>', 'Modified hunks (all)')
+nmap_leader('fM', '<Cmd>Pick git_hunks path="%"<CR>', 'Modified hunks (buf)')
+nmap_leader('fr', '<Cmd>Pick resume<CR>', 'Resume')
+nmap_leader('fR', '<Cmd>Pick lsp scope="references"<CR>', 'References (LSP)')
+nmap_leader('fs', pick_workspace_symbols_live, 'Symbols workspace (live)')
+nmap_leader('fS', '<Cmd>Pick lsp scope="document_symbol"<CR>', 'Symbols document')
+nmap_leader('fv', '<Cmd>Pick visit_paths cwd=""<CR>', 'Visit paths (all)')
+nmap_leader('fV', '<Cmd>Pick visit_paths<CR>', 'Visit paths (cwd)')
+
+-- g is for 'Git'
+local git_log_cmd = [[Git log --pretty=format:\%h\ \%as\ │\ \%s --topo-order]]
+local git_log_buf_cmd = git_log_cmd .. ' --follow -- %'
+nmap_leader('ga', '<Cmd>Git diff --cached<CR>', 'Added diff')
+nmap_leader('gA', '<Cmd>Git diff --cached -- %<CR>', 'Added diff buffer')
+nmap_leader('gc', '<Cmd>Git commit<CR>', 'Commit')
+nmap_leader('gC', '<Cmd>Git commit --amend<CR>', 'Commit amend')
+nmap_leader('gd', '<Cmd>Git diff<CR>', 'Diff')
+nmap_leader('gD', '<Cmd>Git diff -- %<CR>', 'Diff buffer')
+nmap_leader('gl', '<Cmd>' .. git_log_cmd .. '<CR>', 'Log')
+nmap_leader('gL', '<Cmd>' .. git_log_buf_cmd .. '<CR>', 'Log buffer')
+nmap_leader('go', '<Cmd>lua MiniDiff.toggle_overlay()<CR>', 'Toggle overlay')
+nmap_leader('gs', '<Cmd>lua MiniGit.show_at_cursor()<CR>', 'Show at cursor')
+xmap_leader('gs', '<Cmd>lua MiniGit.show_at_cursor()<CR>', 'Show at selection')
+
+-- l is for 'Language'
+
+-- m is for 'Map'
+nmap_leader('mf', '<Cmd>lua MiniMap.toggle_focus()<CR>', 'Focus (toggle)')
+nmap_leader('mr', '<Cmd>lua MiniMap.refresh()<CR>', 'Refresh')
+nmap_leader('ms', '<Cmd>lua MiniMap.toggle_side()<CR>', 'Side (toggle)')
+nmap_leader('mt', '<Cmd>lua MiniMap.toggle()<CR>', 'Toggle')
+
+-- o is for 'Other'
+
+-- s is for 'Session'
+
+-- t is for 'Terminal'
+nmap_leader('tT', '<Cmd>horizontal term<CR>', 'Terminal (horizontal)')
+nmap_leader('tt', '<Cmd>vertical term<CR>', 'Terminal (vertical)')
+
+-- v is for 'Visits'
+local make_pick_core = function(cwd, desc)
+    return function()
+        local sort_latest = MiniVisits.gen_sort.default({ recency_weight = 1 })
+        local local_opts = { cwd = cwd, filter = 'core', sort = sort_latest }
+        MiniExtra.pickers.visit_paths(local_opts, { source = { name = desc } })
+    end
+end
+nmap_leader('vc', make_pick_core('', 'Core visits (all)'), 'Core visits (all)')
+nmap_leader('vC', make_pick_core(nil, 'Core visits (cwd)'), 'Core visits (cwd)')
+nmap_leader('vv', '<Cmd>lua MiniVisits.add_label("core")<CR>', 'Add "core" label')
+nmap_leader('vV', '<Cmd>lua MiniVisits.remove_label("core")<CR>', 'Remove "core" label')
+nmap_leader('vl', '<Cmd>lua MiniVisits.add_label()<CR>', 'Add label')
+nmap_leader('vL', '<Cmd>lua MiniVisits.remove_label()<CR>', 'Remove label')
+
 require('mini.basics').setup({
     options = {
         basic = false,
